cf45c5d1d46de2dc385623f52c67e87f52431b4d
Daniel Chance <dan.chance@outlook.com>
2023-04-16 19:13:44 +0100

    add logic to handle requests to the Auth0 Management API

--- src/config/general.config.ts
+++ src/config/general.config.ts
@@ -16,7 +16,10 @@ const development = {
   auth0: {
     audience: 'https://soundtrack/api',
     domain: 'https://soundtrack.uk.auth0.com/',
-    jwksEndpoint: 'https://soundtrack.uk.auth0.com/.well-known/jwks.json'
+    jwksEndpoint: 'https://soundtrack.uk.auth0.com/.well-known/jwks.json',
+    M2MApplication: {
+      clientId: 'OUMe8qWZ5y62zxiJQjdtaC3Tr0VTNmMW'
+    }
   },
   spotify: {
     apiUrl: 'https://api.spotify.com/v1',
---
+++ src/data_access/auth0.data.ts
@@ -0,0 +1,90 @@
+import { Response } from 'node-fetch';
+import config from '../config/general.config.js';
+import { patch, post, _delete } from '../utils/fetch_wrapper.js';
+
+/**
+ * Define types for the Auth0 Management API responses.
+ */
+
+type AccessTokenResponse = {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+};
+
+/**
+ * Handles all requests to the Auth0 Management API.
+ */
+const auth0API = (() => {
+  /**
+   * Access token required for all requests to the Auth0 Management API.
+   */
+  const accessToken = {
+    value: '',
+    expires: new Date(Date.now() - 1000 * 60)
+  };
+
+  /**
+   * Request an access token from Auth0 for the Management API.
+   */
+  const requestAccessToken = async () => {
+    if (accessToken.value && accessToken.expires > new Date()) {
+      return;
+    }
+    const url = `${config.auth0.domain}oauth/token`;
+    const body = new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: config.auth0.M2MApplication.clientId,
+      client_secret: `${process.env.SOUNDTRACKM2M_CLIENT_SECRET}`,
+      audience: `${config.auth0.domain}api/v2/`
+    });
+    const headers = {
+      'content-type': 'application/x-www-form-urlencoded'
+    };
+    const res = await post<AccessTokenResponse>(url, body, { headers });
+    accessToken.value = res.access_token;
+    accessToken.expires = new Date(Date.now() + res.expires_in * 1000);
+  };
+
+  /**
+   * Update a users attributes in the Auth0 database.
+   * @param userId Id of the user.
+   * @param updates Attributes to update.
+   */
+  const updateUser = async (
+    userId: string,
+    updates: {
+      username?: string;
+      email?: string;
+      password?: string;
+    }
+  ) => {
+    await requestAccessToken();
+    await patch(
+      `${config.auth0.domain}api/v2/users/${userId}1`,
+      JSON.stringify(updates),
+      {
+        headers: {
+          'content-type': 'application/json',
+          authorization: `Bearer ${accessToken.value}`
+        }
+      }
+    );
+  };
+
+  /**
+   * Delete a user from the Auth0 database.
+   * @param userId Id of the user.
+   */
+  const deleteUser = async (userId: string) => {
+    await requestAccessToken();
+    await _delete(`${config.auth0.domain}api/v2/users/${userId}`);
+  };
+
+  return {
+    updateUser,
+    deleteUser
+  };
+})();
+
+export default auth0API;
